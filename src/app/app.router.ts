abf47ddb34df331b07e514bc7eec39f6da584c85
Drex72 <femidavooo@gmail.com>
2024-04-24 11:23:47 +0100

    logging

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -13,7 +13,7 @@ appRouter.use("/programs", programRouter)
 appRouter.use("/profile", profileRouter)
 
 appRouter.get("/health", (req, res) => {
-     console.log(req.isPaused, req.socket.remoteAddress)
+     console.log(req.ip, req.socket.remoteAddress)
     res.status(HttpStatus.OK).json({
         message: "Api up",
         version: "1.0",
